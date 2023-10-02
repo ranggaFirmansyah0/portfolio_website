bebbb90b7d9e03cf84cd55088808279c3b2b104a
ranggaFirmansyah0 <ranggafirmansyah.pfs@gmail.com>
2023-10-02 10:30:55 +0000

    Add favicon and modif  About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,13 @@
 export default function About(){
 	return(
-		<div id="about" className="h-screen text-white">
-			<div>About</div>
+		<div id="about" className="h-screen flex flex-col items-center text-[#f5f5f5] pt-[130px]">
+		<div className="text-2xl font-bold mb-4">About</div>
+		<p className="max-w-[740px] text-[20px] opacity-80 leading-7 text-justify">
+			My name is Rangga Firmansyah.
+			I am a graduate of a Vocational High School majoring in Software Engineering.
+			I am a Frontend Web Developer.
+			I have internship experience as a Frontend Web Developer for 4 months at Muslim Go Online (a digital marketing consulting company).
+		</p>
 		</div>
-	)
+		)
 }
