d22c6a57c44a15f3ceab9a5f9feac5499c0789e2
ranggaFirmansyah0 <ranggafirmansyah.pfs@gmail.com>
2023-10-18 08:08:44 +0000

    edit project page and project card

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,40 +1,47 @@
+import React from 'react';
+import { AiFillHtml5 } from 'react-icons/ai';
 import { BsCodeSlash, BsPlay } from 'react-icons/bs';
 import { SiTailwindcss } from 'react-icons/si';
 import { GrReactjs } from 'react-icons/gr';
 
-import project1 from "../images/project1.png";
-
 const iconSize = 30;
 
-const ReactJs = () => <GrReactjs size={iconSize} color={"#61DAFB"} title="React JS" />;
-const Tailwind = () => <SiTailwindcss size={iconSize} color={"#06B6D4"} title="Tailwind CSS" />;
+const stackComponents = {
+  ReactJS: <GrReactjs size={iconSize} color={"#61DAFB"} title="React JS" />,
+  Tailwind: <SiTailwindcss size={iconSize} color={"#06B6D4"} title="Tailwind CSS" />,
+  HTML: <AiFillHtml5 size={iconSize} color={"#FF5722"} title="HTML5" />
+};
 
-export default function ProjectCard() {
-	return (
-			<div className="flex gap-6 p-4 bg-slate-500/25">
-				<img src={project1} className="w-[340px] h-[190px] bg-[#f5f5f5] rounded-sm object-center border-2 border-zinc-400" />
-				<div className="p-1 flex flex-col justify-between">
-					<div>
-						<div className="font-bold text-[18px] text-[#ffffff]">
-							Personal Portfolio Website
-							<span className="ml-2 font-light">/</span> 
-							<span className="font-light italic text-[14px]">Personal Project</span>
-						</div>
-						<div className="mt-1">This is my first personal portfolio website. I build this from scratch.</div>
-					</div>
-					<div className="flex items-center gap-2">
-						Stack : 
-						<ReactJs />
-						<Tailwind />
-					</div>
-					<div className="flex gap-4 text-[14px]">
-						<a href="" className="flex gap-1 items-center p-2 rounded-sm border-[#f5f5f5] border-2 hover:text-black/80 hover:bg-[#14ffec] hover:border-[#14ffec]"><BsCodeSlash size={20} /> Source Code</a>
-						<a href="" className="flex items-center p-2 rounded-sm border-[#f5f5f5] border-2 hover:text-black/80 hover:bg-[#14ffec] hover:border-[#14ffec]">
-							<BsPlay size={25} /> 
-							Live Demo
-						</a>
-					</div>
-				</div>
-			</div>
-		)
+export default function ProjectCard({name, type, desc, stack, thumbnail, githubLink, demoLink}) {
+  const stackArray = stack.split(',').map(item => item.trim());
+  const jsxElements = stackArray.map((item, index) => {
+    const StackComponent = stackComponents[item];
+    return StackComponent ? React.cloneElement(StackComponent, { key: index }) : null;
+  });
+
+  return (
+    <div className="flex gap-6 p-4 bg-slate-500/25">
+      <img src={thumbnail} className="w-[340px] h-[190px] bg-[#f5f5f5] rounded-sm object-center border-2 border-zinc-400" />
+      <div className="p-1 flex flex-col justify-between">
+        <div>
+          <div className="font-bold text-[18px] text-[#ffffff]">
+            {name}
+            <span className="ml-2 font-light">/</span>
+            <span className="font-light italic text-[14px]">{type}</span>
+          </div>
+          <div className="mt-1">{desc}</div>
+        </div>
+        <div className="flex items-center gap-2">
+          Stack: {jsxElements}
+        </div>
+        <div className="flex gap-4 text-[14px]">
+          <a href={githubLink} target="_blank" className="flex gap-1 items-center p-2 rounded-sm border-[#f5f5f5] border-2 hover:text-black/80 hover:bg-[#14ffec] hover:border-[#14ffec]"><BsCodeSlash size={20} /> Source Code</a>
+          <a href={demoLink} className="flex items-center p-2 rounded-sm border-[#f5f5f5] border-2 hover:text-black/80 hover:bg-[#14ffec] hover:border-[#14ffec]">
+            <BsPlay size={25} />
+            Live Demo
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,3 +1,5 @@
+import project1 from "../images/project1.png";
+
 import ProjectCard from '../components/ProjectCard.jsx';
 
 export default function Project() {
@@ -6,7 +8,15 @@ export default function Project() {
 
 			<div className="text-2xl font-bold mb-16 border-[#14ffec] border-b-2">Projects</div>
 			<div className="w-full max-w-[80%] flex flex-col gap-4">
-				<ProjectCard />
+				<ProjectCard 
+					name="Portfolio Website" 
+					type="Personal Project" 
+					desc="This is my first portfolio website. I build this from scratch." 
+					stack="ReactJS, Tailwind" 
+					thumbnail={project1}
+					githubLink="https://github.com/ranggaFirmansyah0/portfolio_website"
+					demoLink="#"
+				/>
 			</div>
 
 		</div>
