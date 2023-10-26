4010453ba86471001a6e214142b61b9198c4f7aa
ranggaFirmansyah0 <ranggafirmansyah.pfs@gmail.com>
2023-10-26 14:41:54 +0000

    add react-scroll and edit Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-scroll';
 import ranggaLogo from '../images/rangga-logo.png';
 import { BsLinkedin, BsGithub } from 'react-icons/bs';
 
@@ -8,9 +9,39 @@ export default function Navbar() {
 				<img src={ranggaLogo} className="w-14 mx-auto"/>
 			</div>
 			<ul className="flex gap-6 flex-1 justify-center">
-				<li className="hover:font-medium hover:text-white"><a href="#home">Home</a></li>
-				<li className="hover:font-medium hover:text-white"><a href="#about">About</a></li>
-				<li className="hover:font-medium hover:text-white"><a href="#project">Project</a></li>
+				<li className="hover:font-medium hover:text-white">
+					<Link 
+						to="home" 
+						spy={true} 
+						smooth={true} 
+						duration={100}
+						className="cursor-pointer"
+						>
+							Home
+					</Link>
+				</li>
+				<li className="hover:font-medium hover:text-white">
+					<Link 
+						to="about" 
+						spy={true} 
+						smooth={true} 
+						duration={100}
+						className="cursor-pointer"
+						>
+							About
+					</Link>
+				</li>
+				<li className="hover:font-medium hover:text-white">
+					<Link 
+						to="project" 
+						spy={true} 
+						smooth={true} 
+						duration={100}
+						className="cursor-pointer"
+						>
+							Project
+					</Link>
+				</li>
 			</ul>
 			<div className="flex gap-4 items-center flex-1 justify-center">
 				<a href="" className="hover:opacity-100 opacity-90">
@@ -19,7 +50,15 @@ export default function Navbar() {
 				<a href="" className="hover:opacity-100 opacity-90">
 					<BsGithub size="24" />
 				</a>
-				<a href="#contact" className="bg-[#f5f5f5] text-[#212121] py-1 px-2 rounded-sm opacity-90 hover:opacity-100">Contact Me</a>
+				<Link 
+					to="contact" 
+					spy={true} 
+					smooth={true} 
+					duration={100}
+					className="cursor-pointer bg-[#f5f5f5] text-[#212121] py-1 px-2 rounded-sm opacity-90 hover:opacity-100"
+					>
+						Contact Me
+				</Link>
 			</div>
 		</div>
 	);
