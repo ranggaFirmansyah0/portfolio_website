cde4404f564278c0b206beee73ccdf89b6a7e614
ranggaFirmansyah0 <ranggafirmansyah.pfs@gmail.com>
2023-10-29 15:47:13 +0000

    add navbar responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,35 @@
+import React, { useState } from 'react';
 import { Link } from 'react-scroll';
 import ranggaLogo from '../images/rangga-logo.png';
 import { BsLinkedin, BsGithub } from 'react-icons/bs';
 import { GiHamburgerMenu } from 'react-icons/gi';
+import { IoClose } from 'react-icons/io5';
 
 export default function Navbar() {
+	
+	const [menuDisplay, setMenuDisplay] = useState('hidden');
+
+	const showMenu = () => {
+	   setMenuDisplay('flex');
+	};
+
+	const closeMenu = () => {
+	   setMenuDisplay('hidden');
+	};
+
 	return(
 		<div className="fixed w-full bg-[#212121] flex text-[#f5f5f5] py-2 px-2 md:py-6 items-center justify-between z-50">
 			<div className="md:flex-1 flex justify-center items-center">
 				<img src={ranggaLogo} className="w-[13vw] sm:w-12 md:w-14"/>
 			</div>
-			<ul className="hidden md:flex gap-6 md:flex-1 justify-center">
+			<ul className={`${menuDisplay} bg-[#212121] md:flex flex-col absolute right-0 top-0 p-4 text-[5vw] sm:text-[3vw] md:text-[1rem] md:p-0 md:flex-row md:static gap-6 md:flex-1 justify-center`}>
+				<IoClose onClick={closeMenu} className="w-full translate-x-[9px] text-[9vw] mb-4 md:hidden sm:text-[34px] md:text-[3px] opacity-90 hover:opacity-100 cursor-pointer" />
 				<li className="hover:font-medium hover:text-white">
 					<Link 
 						to="home" 
 						spy={true} 
 						smooth={true} 
-						duration={100}
+						duration={300}
 						className="cursor-pointer"
 						>
 							Home
@@ -26,7 +40,7 @@ export default function Navbar() {
 						to="about" 
 						spy={true} 
 						smooth={true} 
-						duration={100}
+						duration={300}
 						className="cursor-pointer"
 						>
 							About
@@ -37,7 +51,7 @@ export default function Navbar() {
 						to="project" 
 						spy={true} 
 						smooth={true} 
-						duration={100}
+						duration={300}
 						className="cursor-pointer"
 						>
 							Project
@@ -55,14 +69,14 @@ export default function Navbar() {
 					to="contact" 
 					spy={true} 
 					smooth={true} 
-					duration={100}
+					duration={300}
 					className="cursor-pointer bg-[#f5f5f5] text-[#212121] py-1 px-2 rounded-sm opacity-90 hover:opacity-100"
 					>
 						Contact Me
 				</Link>
 			</div>
-			<div className="w-[13vw] sm:w-12 md:w-14 flex md:flex-1 md:hidden text-[#f5f5f5] items-center justify-center">
-				<GiHamburgerMenu className="text-[6.5vw] sm:text-[26px] md:text-[30px] opacity-90 hover:opacity-100 cursor-pointer" />
+			<div className="w-[13vw] -z-20 sm:w-12 md:w-14 flex md:flex-1 md:hidden text-[#f5f5f5] items-center justify-center">
+				<GiHamburgerMenu onClick={showMenu} className="text-[6.5vw] sm:text-[26px] md:text-[30px] opacity-90 hover:opacity-100 cursor-pointer" />
 			</div>
 		</div>
 	);
